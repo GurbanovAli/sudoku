f7a70d0617d5ebd7c4f824736e135ab1658c1951
GurbanovAli <50907064+GurbanovAli@users.noreply.github.com>
2019-10-16 23:47:04 +0300

    Update index.js
    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = 
+function solveSudoku(matrix) {
+	const nextCell = findEmpty(matrix);
+	if (!nextCell) return matrix; 
+
+	let [row, col] = nextCell;
+	const values = findValues(row, col, matrix);
+  if (values.length) {
+      for (let value of values) {
+      matrix[row][col] = value;
+			solveSudoku(matrix);
+			if(!findEmpty(matrix)) return matrix;
+		}
+    matrix[row][col] = 0;
+	}
+}
+
+const findEmpty = matrix => {
+	for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+			if (matrix[row][col] === 0) return [row, col];
+		}
+	}
+	return false;
+}
+
+const helper = num => Math.floor(num / 3) * 3;
+
+const findValues = (row, col, matrix) => {
+  const values = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  matrix[row].forEach(el => values.delete(el));
+  for (let row = 0; row < 9; row++) {
+    values.delete(matrix[row][col]);
+  } 
+  for (let r = helper(row), lastRow = r + 3; r < lastRow; r++) {
+    for (let c = helper(col), lastCol = c + 3; c < lastCol; c++) {
+      values.delete(matrix[r][c]);
+    }
+  }
+  return Array.from(values);
 }
